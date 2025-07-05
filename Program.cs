1a832f50716f2366d52e44ed03b1127b09e47db7
rahmaAlmamari <alr816125@gmail.com>
2025-07-05 10:46:20 +0400

    Add full explanation for VerifyPasswordPBKDF2 method

--- Program.cs
+++ Program.cs
@@ -1736,19 +1736,24 @@ namespace MiniProjectBankSystem
         public static bool VerifyPasswordPBKDF2(string password, string savedHash)
         {
             byte[] hashBytes = Convert.FromBase64String(savedHash);
+            //to converts the stored string back into the original 36-byte array
+            //(16 bytes salt + 20 bytes hash).
 
             byte[] salt = new byte[16];
-            Array.Copy(hashBytes, 0, salt, 0, 16);
+            Array.Copy(hashBytes, 0, salt, 0, 16);//to extracts the first 16 bytes (the salt).
 
             var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 100000);
-            byte[] hash = pbkdf2.GetBytes(20);
+            //to recreates the hash for the input password using the same salt and iteration count.
+            byte[] hash = pbkdf2.GetBytes(20);//to gets the expected hash (20 bytes).
 
-            for (int i = 0; i < 20; i++)
+            for (int i = 0; i < 20; i++)//to compares each byte of the newly generated
+                                        //hash with the one stored after the salt.
             {
-                if (hashBytes[i + 16] != hash[i])
+                if (hashBytes[i + 16] != hash[i])//to check if any byte is different
+                                                 //the password is incorrect.
                     return false;
             }
-            return true;
+            return true;//If all bytes match, the password is correct.
         }
         //VerifyPasswordPBKDF2 -> this method verifies a user’s password by:
         // 1. Extracting the original salt from the stored hash
